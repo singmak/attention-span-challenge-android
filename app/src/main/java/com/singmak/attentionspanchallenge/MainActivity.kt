a6aaf5bb44d9b867160237974de3d36f1b9b42f7
Sing Mak <sing.mak@fleetship.com>
2022-02-27 22:28:01 +0800

    initial project

---
+++ app/src/main/java/com/singmak/attentionspanchallenge/MainActivity.kt
@@ -0,0 +1,53 @@
+package com.singmak.attentionspanchallenge
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.singmak.attentionspanchallenge.ui.theme.AttentionSpanChallengeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            App()
+        }
+    }
+}
+
+@Composable
+fun App() {
+    var testCount by remember { mutableStateOf(0) }
+    AttentionSpanChallengeTheme {
+        // A surface container using the 'background' color from the theme
+        Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background) {
+            Greeting("Android")
+            Counter(testCount, onClick = { testCount++ })
+        }
+    }
+
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Composable
+fun Counter(count: Int, onClick: () -> Unit) {
+    Button( onClick = onClick) {
+        Text("clicks counted: $count")
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    AttentionSpanChallengeTheme {
+        Greeting("Android")
+    }
+}
